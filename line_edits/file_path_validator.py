143bb7cf3ce99d6fa5f356e7ddf93f7b84cf0264
Gregory <36880726+gregorycarnegie@users.noreply.github.com>
2023-08-10 16:52:09 +0100

    Update file_path_validator.py

--- line_edits/file_path_validator.py
+++ line_edits/file_path_validator.py
@@ -1,11 +1,11 @@
-from typing import Tuple
+from typing import Optional, Tuple
 
 from PyQt6.QtWidgets import QWidget
 from PyQt6.QtGui import QValidator
 import re
 
 class FilePathValidator(QValidator):
-    def __init__(self, parent: QWidget=None):
+    def __init__(self, parent: Optional[QWidget]=None):
         super(FilePathValidator, self).__init__(parent)
         # Regular expression to match forbidden characters in file paths
         self.invalid_chars_pattern = re.compile(r'[*?<>|]')
@@ -14,4 +14,3 @@ class FilePathValidator(QValidator):
         if self.invalid_chars_pattern.search(a0):
             return QValidator.State.Invalid, a0, a1
         return QValidator.State.Acceptable, a0, a1
-
