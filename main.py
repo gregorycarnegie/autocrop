d10a950b9c4d8a0b02f3bae751a696202270e806
Gregory <36880726+gregorycarnegie@users.noreply.github.com>
2023-08-15 23:28:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from main_widgets import UiMainWindow
 def show_splash_screen() -> QSplashScreen:
     screen = QGuiApplication.primaryScreen()
     screen_height = screen.size().height() if screen is not None else 240
-    splash_pix = QPixmap('resources\\logos\\logo.svg')  # Replace with the path to your image
+    splash_pix = QPixmap('resources\\logos\\logo.svg')
     splash_pix = splash_pix.scaledToHeight(screen_height // 3, Qt.TransformationMode.SmoothTransformation)
     splash = QSplashScreen(splash_pix, Qt.WindowType.WindowStaysOnTopHint)
     splash.show()
@@ -18,9 +18,9 @@ def main() -> None:
     app = QApplication([])
     app.setStyle('Fusion')
     splash = show_splash_screen()
-    QTimer.singleShot(3000, splash.close)  # Show the splash screen for 3 seconds
+    QTimer.singleShot(3_000, splash.close)
     window = UiMainWindow()
-    QTimer.singleShot(3000, window.show)  # Show the main window after 3 secondsS
+    QTimer.singleShot(3_000, window.show)
     app.exec()
 
 if __name__ == '__main__':
