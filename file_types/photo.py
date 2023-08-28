5187c719b5220a755c84d56237aa63933dc7cd0e
Gregory <36880726+gregorycarnegie@users.noreply.github.com>
2023-08-28 15:59:25 +0100

    Update photo.py

--- file_types/photo.py
+++ file_types/photo.py
@@ -36,9 +36,9 @@ class Photo(File):
 
     SAVE_TYPES: ClassVar[Set[str]] = {'.bmp', '.jpg', '.png', '.tiff', '.webp'}
     CV2_TYPES: ClassVar[npt.NDArray[np.str_]] = np.array(['.bmp', '.dib', '.jpeg', '.jpg', '.jpe', '.jp2', '.png',
-                          '.webp', '.pbm', '.pgm', '.ppm', '.pxm', '.pnm', '.pfm',
-                          '.sr', '.ras', '.tiff', '.tif', '.exr', '.hdr', '.pic'])
+                                                          '.webp', '.pbm', '.pgm', '.ppm', '.pxm', '.pnm', '.pfm',
+                                                          '.sr', '.ras', '.tiff', '.tif', '.exr', '.hdr', '.pic'])
     RAW_TYPES: ClassVar[npt.NDArray[np.str_]] = np.array(['.dng', '.arw', '.cr2', '.crw', '.erf',
-                          '.kdc', '.nef', '.nrw', '.orf', '.pef',
-                          '.raf', '.raw', '.sr2', '.srw', '.x3f'])
+                                                          '.kdc', '.nef', '.nrw', '.orf', '.pef',
+                                                          '.raf', '.raw', '.sr2', '.srw', '.x3f'])
     file_types = np.concatenate((CV2_TYPES, RAW_TYPES))
