4fbee8a73c1984b4740c6dff87f60199717741af
Gregory <36880726+gregorycarnegie@users.noreply.github.com>
2023-08-28 12:32:49 +0100

    Update path_type.py

--- line_edits/enums/path_type.py
+++ line_edits/enums/path_type.py
@@ -4,14 +4,14 @@ from enum import auto, Enum, unique
 @unique
 class PathType(Enum):
     """
-Enumeration class for path types.
+    Enumeration class for path types.
 
-Path types include:
-- IMAGE: Represents an image path type.
-- TABLE: Represents a table path type.
-- VIDEO: Represents a video path type.
-- FOLDER: Represents a folder path type.
-"""
+    Path types include:
+    - IMAGE: Represents an image path type.
+    - TABLE: Represents a table path type.
+    - VIDEO: Represents a video path type.
+    - FOLDER: Represents a folder path type.
+    """
 
     IMAGE = auto()
     TABLE = auto()
