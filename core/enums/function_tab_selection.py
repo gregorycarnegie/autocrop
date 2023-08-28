c59a63fd02f13618adb1151763bc088291a9c471
Gregory <36880726+gregorycarnegie@users.noreply.github.com>
2023-08-28 12:46:54 +0100

    Update function_tab_selection.py

--- core/enums/function_tab_selection.py
+++ core/enums/function_tab_selection.py
@@ -4,12 +4,12 @@ from enum import auto, Enum, unique
 @unique
 class FunctionTabSelectionState(Enum):
     """
-Enumeration class for function tab selection states.
+    Enumeration class for function tab selection states.
 
-Function tab selection states include:
-- SELECTED: Represents a selected state.
-- NOT_SELECTED: Represents a not selected state.
-"""
+    Function tab selection states include:
+    - SELECTED: Represents a selected state.
+    - NOT_SELECTED: Represents a not selected state.
+    """
 
     SELECTED = auto()
     NOT_SELECTED = auto()
