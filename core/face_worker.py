9dc7af7bce914e00057eb877b8e20615a723307a
Gregory <36880726+gregorycarnegie@users.noreply.github.com>
2023-08-28 12:44:47 +0100

    Update face_worker.py

--- core/face_worker.py
+++ core/face_worker.py
@@ -6,30 +6,30 @@ import dlib
 
 class FaceWorker:
     """
-A class for performing face-related tasks.
+    A class for performing face-related tasks.
 
-Attributes:
-    LANDMARKS (ClassVar[str]): The path to the shape predictor model file.
-    PROTOTXT (ClassVar[str]): The path to the Caffe model prototxt file.
-    CAFFEMODEL (ClassVar[str]): The path to the Caffe model weights file.
+    Attributes:
+        LANDMARKS (ClassVar[str]): The path to the shape predictor model file.
+        PROTOTXT (ClassVar[str]): The path to the Caffe model prototxt file.
+        CAFFEMODEL (ClassVar[str]): The path to the Caffe model weights file.
 
-Methods:
-    caffe_model(cls) -> cv2.dnn.Net: Returns a pre-trained Caffe model for face detection.
-    worker_tuple(cls) -> Tuple[dlib.fhog_object_detector, dlib.shape_predictor]: Returns a tuple containing the frontal face detector and shape predictor.
+    Methods:
+        caffe_model(cls) -> cv2.dnn.Net: Returns a pre-trained Caffe model for face detection.
+        worker_tuple(cls) -> Tuple[dlib.fhog_object_detector, dlib.shape_predictor]: Returns a tuple containing the frontal face detector and shape predictor.
 
-Example:
-    ```python
-    worker = FaceWorker()
+    Example:
+        ```python
+        worker = FaceWorker()
 
-    # Getting the Caffe model
-    model = worker.caffe_model()
-    print(model)
+        # Getting the Caffe model
+        model = worker.caffe_model()
+        print(model)
 
-    # Getting the worker tuple
-    worker_tuple = worker.worker_tuple()
-    print(worker_tuple)
-    ```
-"""
+        # Getting the worker tuple
+        worker_tuple = worker.worker_tuple()
+        print(worker_tuple)
+        ```
+    """
 
     LANDMARKS: ClassVar[str] = 'resources\\models\\shape_predictor_68_face_landmarks.dat'
     PROTOTXT: ClassVar[str] = 'resources\\weights\\deploy.prototxt.txt'
@@ -38,21 +38,21 @@ Example:
     @classmethod
     def caffe_model(cls) -> cv2.dnn.Net:
         """
-Returns a pre-trained Caffe model for face detection.
+        Returns a pre-trained Caffe model for face detection.
 
-Returns:
-    cv2.dnn.Net: The pre-trained Caffe model for face detection.
-"""
+        Returns:
+            cv2.dnn.Net: The pre-trained Caffe model for face detection.
+        """
 
         return cv2.dnn.readNetFromCaffe(cls.PROTOTXT, cls.CAFFEMODEL)
 
     @classmethod
     def worker_tuple(cls) -> Tuple[dlib.fhog_object_detector, dlib.shape_predictor]:
         """
-Returns a tuple containing the frontal face detector and shape predictor for face detection and landmark localization.
+        Returns a tuple containing the frontal face detector and shape predictor for face detection and landmark localization.
 
-Returns:
-    Tuple[dlib.fhog_object_detector, dlib.shape_predictor]: A tuple containing the frontal face detector and shape predictor.
-"""
+        Returns:
+            Tuple[dlib.fhog_object_detector, dlib.shape_predictor]: A tuple containing the frontal face detector and shape predictor.
+        """
 
         return dlib.get_frontal_face_detector(), dlib.shape_predictor(cls.LANDMARKS)
