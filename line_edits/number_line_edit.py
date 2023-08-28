a78202596805ba77cf20dc84e852dc1820b2e854
Gregory <36880726+gregorycarnegie@users.noreply.github.com>
2023-08-28 12:31:32 +0100

    Update number_line_edit.py

--- line_edits/number_line_edit.py
+++ line_edits/number_line_edit.py
@@ -7,21 +7,21 @@ from .custom_line_edit import CustomLineEdit
 
 class NumberLineEdit(CustomLineEdit):
     """
-Represents a NumberLineEdit class that inherits from the CustomLineEdit class.
+    Represents a NumberLineEdit class that inherits from the CustomLineEdit class.
 
-A custom line edit widget for handling numeric input.
+    A custom line edit widget for handling numeric input.
 
-Args:
-    name (str): The object name of the widget.
-    parent (Optional[QtWidgets.QWidget]): The parent widget. Defaults to None.
+    Args:
+        name (str): The object name of the widget.
+        parent (Optional[QtWidgets.QWidget]): The parent widget. Defaults to None.
 
-Attributes:
-    INVALID_CHARS_PATTERN (ClassVar[Pattern[str]]): A regular expression pattern to match invalid characters in a file path.
+    Attributes:
+        INVALID_CHARS_PATTERN (ClassVar[Pattern[str]]): A regular expression pattern to match invalid characters in a file path.
 
-Methods:
-    validate_path(self): Validates the path by performing color logic and updating the style of the NumberLineEdit.
-    value(self) -> int: Returns the integer value of the text input.
-"""
+    Methods:
+        validate_path(self): Validates the path by performing color logic and updating the style of the NumberLineEdit.
+        value(self) -> int: Returns the integer value of the text input.
+    """
 
     def __init__(self, name: str, parent: Optional[QtWidgets.QWidget] = None):
         super().__init__(parent)
@@ -32,28 +32,28 @@ Methods:
 
     def validate_path(self):
         """
-Validates the path by performing color logic and updating the style of the NumberLineEdit.
+        Validates the path by performing color logic and updating the style of the NumberLineEdit.
 
-Args:
-    self: The NumberLineEdit instance.
+        Args:
+            self: The NumberLineEdit instance.
 
-Returns:
-    None
-"""
+        Returns:
+            None
+        """
 
         self.color_logic(self.text().isdigit())
         self.update_style()
 
     def value(self) -> int:
         """
-Returns the integer value of the text input.
+        Returns the integer value of the text input.
 
-Args:
-    self: The NumberLineEdit instance.
+        Args:
+            self: The NumberLineEdit instance.
 
-Returns:
-    int: The integer value of the text input. If the text cannot be converted to an integer, returns 0.
-"""
+        Returns:
+            int: The integer value of the text input. If the text cannot be converted to an integer, returns 0.
+        """
 
         try:
             return int(self.text())
