89242d11fe6327cad6ecff6b80b2ae1c5b6d6aec
Gregory <36880726+gregorycarnegie@users.noreply.github.com>
2023-08-28 12:33:43 +0100

    Update photo.py

--- file_types/photo.py
+++ file_types/photo.py
@@ -8,31 +8,31 @@ from .file import File
 
 class Photo(File):
     """
-Represents a Photo class that inherits from the File class.
+    Represents a Photo class that inherits from the File class.
 
-The Photo class provides additional functionality for working with photo files. It defines class variables for save types, CV2 types, and RAW types. The `file_types` attribute is a concatenation of CV2 types and RAW types.
+    The Photo class provides additional functionality for working with photo files. It defines class variables for save types, CV2 types, and RAW types. The `file_types` attribute is a concatenation of CV2 types and RAW types.
 
-Attributes:
-    SAVE_TYPES (ClassVar[Set[str]]): A set of file extensions for save types.
-    CV2_TYPES (ClassVar[npt.NDArray[np.str_]]): An array of file extensions for CV2 types.
-    RAW_TYPES (ClassVar[npt.NDArray[np.str_]]): An array of file extensions for RAW types.
-    file_types (npt.NDArray[np.str_]): An array of file types supported by the class.
-    default_directory (str): The default directory for file operations.
+    Attributes:
+        SAVE_TYPES (ClassVar[Set[str]]): A set of file extensions for save types.
+        CV2_TYPES (ClassVar[npt.NDArray[np.str_]]): An array of file extensions for CV2 types.
+        RAW_TYPES (ClassVar[npt.NDArray[np.str_]]): An array of file extensions for RAW types.
+        file_types (npt.NDArray[np.str_]): An array of file types supported by the class.
+        default_directory (str): The default directory for file operations.
 
-Methods:
-    file_filter(): Returns an array of file filters based on the file types.
-    type_string(): Returns a string representing the supported file types.
-    
-Example:
-    ```python
-    # Creating an instance of the Photo class
-    photo = Photo()
+    Methods:
+        file_filter(): Returns an array of file filters based on the file types.
+        type_string(): Returns a string representing the supported file types.
+        
+    Example:
+        ```python
+        # Creating an instance of the Photo class
+        photo = Photo()
 
-    # Getting the file types
-    types = photo.file_types
-    print(types)
-    ```
-"""
+        # Getting the file types
+        types = photo.file_types
+        print(types)
+        ```
+    """
 
     SAVE_TYPES: ClassVar[Set[str]] = {'.bmp', '.jpg', '.png', '.tiff', '.webp'}
     CV2_TYPES: ClassVar[npt.NDArray[np.str_]] = np.array(['.bmp', '.dib', '.jpeg', '.jpg', '.jpe', '.jp2', '.png',
