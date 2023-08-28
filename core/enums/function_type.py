c018ae53fb7fe67d8690a7a6127ae82c4834ddbc
Gregory <36880726+gregorycarnegie@users.noreply.github.com>
2023-08-28 12:46:40 +0100

    Update function_type.py

--- core/enums/function_type.py
+++ core/enums/function_type.py
@@ -3,15 +3,15 @@ from enum import Enum
 
 class FunctionType(Enum):
     """
-Enumeration class for function types.
+    Enumeration class for function types.
 
-Function types include:
-- PHOTO: Represents a photo function type.
-- FOLDER: Represents a folder function type.
-- MAPPING: Represents a mapping function type.
-- VIDEO: Represents a video function type.
-- FRAME: Represents a frame function type.
-"""
+    Function types include:
+    - PHOTO: Represents a photo function type.
+    - FOLDER: Represents a folder function type.
+    - MAPPING: Represents a mapping function type.
+    - VIDEO: Represents a video function type.
+    - FRAME: Represents a frame function type.
+    """
 
     PHOTO = 0
     FOLDER = 1
