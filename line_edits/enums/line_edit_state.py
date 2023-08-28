04f8bd96b799f04b8719847e6e2a15427c940e50
Gregory <36880726+gregorycarnegie@users.noreply.github.com>
2023-08-28 12:33:04 +0100

    Update line_edit_state.py

--- line_edits/enums/line_edit_state.py
+++ line_edits/enums/line_edit_state.py
@@ -4,12 +4,12 @@ from enum import auto, Enum, unique
 @unique
 class LineEditState(Enum):
     """
-Enumeration class for line edit states.
+    Enumeration class for line edit states.
 
-Line edit states include:
-- VALID_INPUT: Represents a valid input state.
-- INVALID_INPUT: Represents an invalid input state.
-"""
+    Line edit states include:
+    - VALID_INPUT: Represents a valid input state.
+    - INVALID_INPUT: Represents an invalid input state.
+    """
 
     VALID_INPUT = auto()
     INVALID_INPUT = auto()
