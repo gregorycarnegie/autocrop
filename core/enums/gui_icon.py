0ac4722c57da9d9b901d40e09ee2041334133734
Gregory <36880726+gregorycarnegie@users.noreply.github.com>
2023-12-03 13:29:04 +0000

    resource_path fn
    added resource_path function to process resource location

--- core/enums/gui_icon.py
+++ core/enums/gui_icon.py
@@ -1,34 +1,34 @@
 from enum import Enum
-from typing import Tuple
+from pathlib import Path
+import sys
+
+def resource_path(relative_path: str) -> str:
+    base_path = getattr(sys, '_MEIPASS2', Path().resolve())
+
+    return (Path(base_path) / relative_path).as_posix()
 
 
 class GuiIcon(Enum):
-    CLAPPERBOARD = 'resources\\icons\\clapperboard.svg'
-    CANCEL = 'resources\\icons\\cancel.svg'
-    CROP = 'resources\\icons\\crop.svg'
-    EXCEL = 'resources\\icons\\excel.svg'
-    FOLDER = 'resources\\icons\\folder.svg'
-    LOGO = 'resources\\logos\\logo.svg'
-    MULTIMEDIA_MUTE = 'resources\\icons\\multimedia_mute.svg'
-    MULTIMEDIA_PAUSE = 'resources\\icons\\multimedia_pause.svg'
-    MULTIMEDIA_PLAY = 'resources\\icons\\multimedia_play.svg'
-    MULTIMEDIA_UNMUTE = 'resources\\icons\\multimedia_unmute.svg'
-    MULTIMEDIA_STOP = 'resources\\icons\\multimedia_stop.svg'
-    MULTIMEDIA_LEFT = 'resources\\icons\\multimedia_left.svg'
-    MULTIMEDIA_RIGHT = 'resources\\icons\\multimedia_right.svg'
-    MULTIMEDIA_REWIND = 'resources\\icons\\multimedia_rewind.svg'
-    MULTIMEDIA_FASTFWD = 'resources\\icons\\multimedia_fastfwd.svg'
-    MULTIMEDIA_BEGINING = 'resources\\icons\\multimedia_begining.svg'
-    MULTIMEDIA_END = 'resources\\icons\\multimedia_end.svg'
-    MULTIMEDIA_LEFTMARKER = 'resources\\icons\\multimedia_leftmarker.svg'
-    MULTIMEDIA_RIGHTMARKER = 'resources\\icons\\multimedia_rightmarker.svg'
-    MULTIMEDIA_CROPVIDEO = 'resources\\icons\\crop_video.svg'
-    MULTIMEDIA_LABEL_A = 'resources\\icons\\marker_label_a.svg'
-    MULTIMEDIA_LABEL_B = 'resources\\icons\\marker_label_b.svg'
-    PICTURE = 'resources\\icons\\picture.svg'
-    # RADIO_NONE: Tuple = 'resources/icons/file_no_checked.svg', 'resources/icons/file_no_unchecked.svg'
-    # RADIO_BMP: Tuple = 'resources/icons/file_bmp_checked.svg', 'resources/icons/file_bmp_unchecked.svg'
-    # RADIO_JPG: Tuple = 'resources/icons/file_jpg_checked.svg', 'resources/icons/file_jpg_unchecked.svg'
-    # RADIO_PNG: Tuple = 'resources/icons/file_png_checked.svg', 'resources/icons/file_png_unchecked.svg'
-    # RADIO_TIFF: Tuple = 'resources/icons/file_tiff_checked.svg', 'resources/icons/file_tiff_unchecked.svg'
-    # RADIO_WEBP: Tuple = 'resources/icons/file_webp_checked.svg', 'resources/icons/file_webp_unchecked.svg'
+    CLAPPERBOARD = resource_path('resources\\icons\\clapperboard.svg')
+    CANCEL = resource_path('resources\\icons\\cancel.svg')
+    CROP = resource_path('resources\\icons\\crop.svg')
+    EXCEL = resource_path('resources\\icons\\excel.svg')
+    FOLDER = resource_path('resources\\icons\\folder.svg')
+    LOGO = resource_path('resources\\logos\\logo.svg')
+    MULTIMEDIA_MUTE = resource_path('resources\\icons\\multimedia_mute.svg')
+    MULTIMEDIA_PAUSE = resource_path('resources\\icons\\multimedia_pause.svg')
+    MULTIMEDIA_PLAY = resource_path('resources\\icons\\multimedia_play.svg')
+    MULTIMEDIA_UNMUTE = resource_path('resources\\icons\\multimedia_unmute.svg')
+    MULTIMEDIA_STOP = resource_path('resources\\icons\\multimedia_stop.svg')
+    MULTIMEDIA_LEFT = resource_path('resources\\icons\\multimedia_left.svg')
+    MULTIMEDIA_RIGHT = resource_path('resources\\icons\\multimedia_right.svg')
+    MULTIMEDIA_REWIND = resource_path('resources\\icons\\multimedia_rewind.svg')
+    MULTIMEDIA_FASTFWD = resource_path('resources\\icons\\multimedia_fastfwd.svg')
+    MULTIMEDIA_BEGINING = resource_path('resources\\icons\\multimedia_begining.svg')
+    MULTIMEDIA_END = resource_path('resources\\icons\\multimedia_end.svg')
+    MULTIMEDIA_LEFTMARKER = resource_path('resources\\icons\\multimedia_leftmarker.svg')
+    MULTIMEDIA_RIGHTMARKER = resource_path('resources\\icons\\multimedia_rightmarker.svg')
+    MULTIMEDIA_CROPVIDEO = resource_path('resources\\icons\\crop_video.svg')
+    MULTIMEDIA_LABEL_A = resource_path('resources\\icons\\marker_label_a.svg')
+    MULTIMEDIA_LABEL_B = resource_path('resources\\icons\\marker_label_b.svg')
+    PICTURE = resource_path('resources\\icons\\picture.svg')
